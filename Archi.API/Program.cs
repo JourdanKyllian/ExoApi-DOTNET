32895518a6bd1545aa7153ed5c312cc01144bbc5
Zine Dev <zhrdev354@gmail.com>
2026-02-12 15:06:50 +0100

    adding log files outut

--- Archi.API/Program.cs
+++ Archi.API/Program.cs
@@ -1,42 +1,63 @@
 using Archi.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using Serilog.Events;
 using Asp.Versioning;  
 
 Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(
+        theme: Serilog.Sinks.SystemConsole.Themes.SystemConsoleTheme.Colored,
+        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .WriteTo.File(
+        path: "logs/log-api-s.txt",
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 7,
+        outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {Message:lj}{NewLine}{Exception}")
     .CreateLogger();
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Host.UseSerilog();
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<ArchiDbContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("archilogdb")));
-
-builder.Services.AddApiVersioning(options =>
+try
 {
-    options.DefaultApiVersion = new ApiVersion(1, 0);
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.ReportApiVersions = true;
-    options.ApiVersionReader = new UrlSegmentApiVersionReader(); 
-});
+    var builder = WebApplication.CreateBuilder(args);
 
+    builder.Host.UseSerilog();
 
+    builder.Services.AddControllers();
+    builder.Services.AddDbContext<ArchiDbContext>(options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("archilogdb")));
 
-builder.Services.AddOpenApi();
+    builder.Services.AddApiVersioning(options =>
+    {
+        options.DefaultApiVersion = new ApiVersion(1, 0);
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ReportApiVersions = true;
+        options.ApiVersionReader = new UrlSegmentApiVersionReader(); 
+    });
 
-var app = builder.Build();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddOpenApi();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseSerilogRequestLogging();
+    app.MapControllers();
+
+    Log.Information("Démarrage de l'API Archi v1.0");
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseSerilogRequestLogging();
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "L'application a échoué au démarrage");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
